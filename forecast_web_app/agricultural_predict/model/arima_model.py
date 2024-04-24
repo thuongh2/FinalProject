856f95e4335e5ce008a5344d00c4fe2183b3a65f
Thuong Vo Hoai <hoaithuong.data@gmail.com>
2024-04-24 17:21:45 +0700

    ++ add predict model

--- forecast_web_app/agricultural_predict/model/arima_model.py
+++ forecast_web_app/agricultural_predict/model/arima_model.py
@@ -1,10 +1,49 @@
+import joblib
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.stattools import acf
+from sklearn.metrics import r2_score
 
-def predict():
+
+def predict(model_url, n_periods, test_data):
+    model_load = joblib.load(model_url)
+    if model_load:
+        fc, confint = model_load.predict(n_periods=n_periods, return_conf_int=True, dynamic=True)
+        index_of_fc = np.arange(len(test_data), len(test_data.values) + n_periods)
+        fc_series = pd.Series(fc, index=test_data.index)
+        return fc_series
+    else:
+        raise Exception("Không tìm thấy model")
+
+
+def train_model():
     pass
-
 
 
 def evaluate_mode():
     pass
 
 
+
+def train_for_upload_mode(model_url, n_periods, test_data):
+    fc = predict(model_url, n_periods, test_data)
+    score = forecast_accuracy(fc.values, test_data.price.values)
+    print(score)
+    return fc, score
+    
+
+def forecast_accuracy(forecast, actual):
+    mape = np.mean(np.abs(forecast - actual)/np.abs(actual))  # MAPE
+    me = np.mean(forecast - actual)             # ME
+    mae = np.mean(np.abs(forecast - actual))    # MAE
+    mpe = np.mean((forecast - actual)/actual)   # MPE
+    rmse = np.mean((forecast - actual)**2)**.5  # RMSE
+    r2 = r2_score(actual, forecast) 
+    return({'mape':mape, 'me':me, 'mae': mae,
+            'mpe': mpe, 'rmse':rmse,  'r2': r2})
+    
+    
+if __name__ == '__main__':
+    test_data = pd.read_csv("../test_data/test_data_arima.csv")
+    model_url = "../test_data/arima.joblib"
+    train_for_upload_mode(model_url, 599, test_data)
---
+++ forecast_web_app/agricultural_predict/model/var_model.py
@@ -0,0 +1,79 @@
+import joblib
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.stattools import acf
+
+
+def invert_transformation(df_train, df_forecast, second_diff=False):
+    """Revert back the differencing to get the forecast to original scale."""
+    df_fc = df_forecast.copy()
+    columns = df_train.columns
+    for col in columns:
+        # Roll back 2nd Diff
+        if second_diff:
+            df_fc[str(col)+'_1d'] = (df_train[col].iloc[-1]-df_train[col].iloc[-2]) + df_fc[str(col)+'_2d'].cumsum()
+        # Roll back 1st Diff
+        df_fc[str(col)+'_forecast'] = df_train[col].iloc[-1] + df_fc[str(col)+'_1d'].cumsum()
+    return df_fc
+
+
+
+def predict(model_url, n_periods, train_data, test_data):
+    model_load = joblib.load(model_url)
+    if model_load:
+        lag_order = model_load.k_ar
+        print(lag_order)
+        # Input data for forecasting
+        forecast_input = convert_to_stationary(train_data).values[-lag_order:]
+        print(forecast_input)
+        
+        fc = model_load.forecast(y=forecast_input, steps=n_periods)
+        df_forecast = pd.DataFrame(fc, index=test_data.index[-n_periods:], columns=test_data.columns + '_2d')
+        df_results = invert_transformation(train_data, df_forecast, second_diff=True)
+        df_results.loc[:, ['price_forecast']]
+        return df_results
+    else:
+        raise Exception("Không tìm thấy model")
+
+
+def convert_to_stationary(df_train, lags=1):
+    df_differenced = df_train.diff().dropna()
+    return df_differenced
+
+def train_model():
+    pass
+
+
+def evaluate_mode():
+    pass
+
+
+def forecast_accuracy(forecast, actual):
+    mape = np.mean(np.abs(forecast - actual)/np.abs(actual))  # MAPE
+    me = np.mean(forecast - actual)             # ME
+    mae = np.mean(np.abs(forecast - actual))    # MAE
+    mpe = np.mean((forecast - actual)/actual)   # MPE
+    rmse = np.mean((forecast - actual)**2)**.5  # RMSE
+    r2 = 0
+    return({'mape':mape, 'me':me, 'mae': mae,
+            'mpe': mpe, 'rmse':rmse,  'r2': r2})
+    
+    
+def train_for_upload_mode(model_url, n_periods, train_data, test_data):
+    
+    fc = predict(model_url, n_periods, train_data, test_data)
+    print(fc.price_forecast.values)
+    score = forecast_accuracy(fc.price_forecast.values, test_data.price[:n_periods].values)
+    print(score)
+    return fc, score
+    
+      
+if __name__ == '__main__':
+    test_data = pd.read_csv("../test_data/test_var_model.csv")
+    train_data = pd.read_csv("../test_data/train_var_model.csv")
+    train_data['date'] = pd.to_datetime(train_data['date'])
+    train_data = train_data.set_index('date')
+    test_data['date'] = pd.to_datetime(test_data['date'])
+    test_data = test_data.set_index('date')
+    model_url = "../test_data/var.joblib"
+    train_for_upload_mode(model_url, 30,train_data, test_data)

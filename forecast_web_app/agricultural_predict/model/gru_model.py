3001472e238792f920b9576f6435934a04c34371
DESKTOP-3H6GUSO\haonh <haonhi.data@gmail.com>
2024-05-20 22:13:34 +0700

    train_model_rnn: scaler multi-data

--- forecast_web_app/agricultural_predict/model/gru_model.py
+++ forecast_web_app/agricultural_predict/model/gru_model.py
@@ -81,6 +81,123 @@ class GRUModel(BaseModel):
         data_predicted = pd.concat([original_df, predicted_df], ignore_index=True)
         
         return data_predicted
+    
+    def train_model(self, argument):
+        """
+        Train model in web
+        params argument
+            size: size split data
+            layer: list of layer configurations
+            epoch: number of epochs
+            batch_size: size of the batch
+        return LSTM MODEL
+        """
+        def create_sequences(data, time_step):
+            X, y = [], []
+            for i in range(len(data) - time_step):
+                X.append(data[i:(i + time_step)])
+                y.append(data[i + time_step])
+            return np.array(X), np.array(y)
+
+        # Prepare data
+        if argument.get('size', 0.8) is None:
+            raise Exception("Size is required")
+
+        self.data = pd.read_csv(self.data_uri)
+        self._clean_data()
+
+        if self.data.shape[1] == 2:  # Only index and price column
+            scaler_price = MinMaxScaler(feature_range=(0, 1))
+            scaled_price = scaler_price.fit_transform(self.data[self.PRICE_COLUMN].values.reshape(-1, 1))
+
+            time_step = argument.get('timestep', 10)
+            self.X, self.y = create_sequences(scaled_price, time_step)
+            input_shape = (time_step, 1)
+        else:
+            # Scale each column separately
+            scaler_price = MinMaxScaler(feature_range=(0, 1))
+            scaled_price = scaler_price.fit_transform(self.data[self.PRICE_COLUMN].values.reshape(-1, 1))
+
+            other_columns = self.data.drop(columns=[self.PRICE_COLUMN])
+            scalers_other = {}
+            scaled_other = np.zeros(other_columns.shape)
+            for i, col in enumerate(other_columns.columns):
+                scalers_other[col] = MinMaxScaler(feature_range=(0, 1))
+                scaled_other[:, i] = scalers_other[col].fit_transform(other_columns[col].values.reshape(-1, 1)).flatten()
+
+            scaled_data = np.concatenate((scaled_price, scaled_other), axis=1)
+
+            time_step = argument.get('timestep', 10)
+            self.X, self.y = create_sequences(scaled_data, time_step)
+            input_shape = (time_step, scaled_data.shape[1])
+            
+        train_size = int(len(self.data) * argument['size'])
+        self.X_train, self.X_test = self.X[:train_size], self.X[train_size:]
+        self.y_train, self.y_test = self.y[:train_size], self.y[train_size:]
+
+        self.dates = self.data.index[time_step:]
+        self.train_dates, self.test_dates = self.dates[:train_size], self.dates[train_size:]
+
+        logging.info('Start train LSTM MODEL')
+
+        # Create model
+        model = Sequential()
+        layers_data = argument.get('layers_data', [{'id': 0, 'units': 64}])
+        layers_data = [{'id': layer['id'], 'units': int(layer['units'])} for layer in layers_data]
+
+        if len(layers_data) == 1:
+            units = layers_data[0]['units']
+            model.add(LSTM(units, return_sequences=False, input_shape=input_shape))
+            model.add(Dropout(0.2))
+        else:
+            for i, layer in enumerate(layers_data):
+                units = layer['units']
+                if i == 0:
+                    model.add(LSTM(units, return_sequences=True, input_shape=(time_step, 1)))
+                    model.add(Dropout(0.2))
+                elif i == len(layers_data) - 1:
+                    model.add(LSTM(units, return_sequences=False))
+                    model.add(Dropout(0.2))
+                else:
+                    model.add(LSTM(units, return_sequences=True))
+                    model.add(Dropout(0.2))
+        model.add(Dense(1))
+
+        optimizer = Adam()
+        model.compile(optimizer=optimizer, loss='mean_squared_error')
+
+        epochs = argument['epochs']
+        batchsize = argument.get('batchsize', 64)
+        print(f"Training Parameters:\n Epochs: {epochs}\n Batch size: {batchsize}\n Time step: {time_step}\n Size: {argument['size']}\n")
+        
+        model.fit(self.X_train, self.y_train, epochs=epochs, batch_size=batchsize)
+
+        print("Model Summary:")
+        model.summary()
+        
+        self.model = model
+
+        # Predict
+        X_test_predict = model.predict(self.X_test)
+
+        X_test_predict = scaler_price.inverse_transform(X_test_predict)
+        y_test_actual = scaler_price.inverse_transform(self.y_test)
+
+        self.y_test_actual = y_test_actual
+
+        self.actual_data = pd.DataFrame({
+            'date': self.test_dates,
+            'price': y_test_actual.flatten()
+        }).set_index('date')
+
+        self.forecast_data = pd.DataFrame({
+            'date': self.test_dates,
+            'price': X_test_predict.flatten()
+        }).set_index('date')
+
+        self.accuracy = self.forecast_accuracy(X_test_predict, y_test_actual)
+
+        return self.forecast_data, self.accuracy, self.model
 
 
     
--- forecast_web_app/agricultural_predict/model/lstm_model.py
+++ forecast_web_app/agricultural_predict/model/lstm_model.py
@@ -109,11 +109,31 @@ class LSTMModel(BaseModel):
         self.data = pd.read_csv(self.data_uri)
         self._clean_data()
 
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaled_price = scaler.fit_transform(self.data[self.PRICE_COLUMN].values.reshape(-1, 1))
+        if self.data.shape[1] == 2:  # Only index and price column
+            scaler_price = MinMaxScaler(feature_range=(0, 1))
+            scaled_price = scaler_price.fit_transform(self.data[self.PRICE_COLUMN].values.reshape(-1, 1))
 
-        time_step = argument.get('timestep', 10)
-        self.X, self.y = create_sequences(scaled_price, time_step)
+            time_step = argument.get('timestep', 10)
+            self.X, self.y = create_sequences(scaled_price, time_step)
+            input_shape = (time_step, 1)
+        else:
+            # Scale each column separately
+            scaler_price = MinMaxScaler(feature_range=(0, 1))
+            scaled_price = scaler_price.fit_transform(self.data[self.PRICE_COLUMN].values.reshape(-1, 1))
+
+            other_columns = self.data.drop(columns=[self.PRICE_COLUMN])
+            scalers_other = {}
+            scaled_other = np.zeros(other_columns.shape)
+            for i, col in enumerate(other_columns.columns):
+                scalers_other[col] = MinMaxScaler(feature_range=(0, 1))
+                scaled_other[:, i] = scalers_other[col].fit_transform(other_columns[col].values.reshape(-1, 1)).flatten()
+
+            scaled_data = np.concatenate((scaled_price, scaled_other), axis=1)
+
+            time_step = argument.get('timestep', 10)
+            self.X, self.y = create_sequences(scaled_data, time_step)
+            input_shape = (time_step, scaled_data.shape[1])
+            
         train_size = int(len(self.data) * argument['size'])
         self.X_train, self.X_test = self.X[:train_size], self.X[train_size:]
         self.y_train, self.y_test = self.y[:train_size], self.y[train_size:]
@@ -130,7 +150,7 @@ class LSTMModel(BaseModel):
 
         if len(layers_data) == 1:
             units = layers_data[0]['units']
-            model.add(LSTM(units, return_sequences=False, input_shape=(time_step, 1)))
+            model.add(LSTM(units, return_sequences=False, input_shape=input_shape))
             model.add(Dropout(0.2))
         else:
             for i, layer in enumerate(layers_data):
@@ -163,8 +183,8 @@ class LSTMModel(BaseModel):
         # Predict
         X_test_predict = model.predict(self.X_test)
 
-        X_test_predict = scaler.inverse_transform(X_test_predict)
-        y_test_actual = scaler.inverse_transform(self.y_test)
+        X_test_predict = scaler_price.inverse_transform(X_test_predict)
+        y_test_actual = scaler_price.inverse_transform(self.y_test)
 
         self.y_test_actual = y_test_actual
 

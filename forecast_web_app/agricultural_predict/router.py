4a426bc052f23bd699b0d821dd2c8fb81d661496
Thuong Vo Hoai <hoaithuong.data@gmail.com>
2024-05-25 21:37:03 +0700

    feat: add render price forecast in home page

--- forecast_web_app/agricultural_predict/router.py
+++ forecast_web_app/agricultural_predict/router.py
@@ -8,20 +8,25 @@ from flask import current_app
 from config.db import db, model, user, train_model
 import requests
 import pandas as pd
+import datetime
 
 main_router = Blueprint('main_router', __name__, static_folder='static',
-            template_folder='templates')
+                        template_folder='templates')
+
+AGRICULTURAL_TYPES = ['LUA', 'CAFE']
+AGRICULTURAL_TYPES_MAPPING = {'LUA': 'Lúa', 'CAFE': 'Cà phê'}
+
 
 @main_router.route('/')
 def index():
     records = list(model.find())
     records_data = list(train_model.find())
     price_data = []
-    seen_algricutural_names = set()
-
+    seen_agricultural_names = set()
+    price_agricultural = get_price_with_date()
     for record_data in records_data:
         data_url = record_data.get('data_name')
-        if record_data.get('algricutural_name') not in seen_algricutural_names:
+        if record_data.get('algricutural_name') not in seen_agricultural_names:
             response = requests.get(data_url)
 
             if response.status_code == 200:
@@ -33,8 +38,7 @@ def index():
                         'price': row['price'],
                         'algricutural_name': record_data.get('algricutural_name')
                     })
-                seen_algricutural_names.add(record_data.get('algricutural_name'))
-
+                seen_agricultural_names.add(record_data.get('algricutural_name'))
 
     model_name = request.args.get('model_name', 'LSTM')
     agricultural_type = request.args.get('agricultural_type', 'CAFE')
@@ -43,14 +47,50 @@ def index():
     model_names = [m.get('name') for m in model_data]
     current_app.logger.info(model_names)
 
-    if(model_name):
+    if (model_name):
         model_data_find = model.find_one({'name': model_name})
         attrs = model_data_find.get('attrs')
         data = attrs.get('data', [])
         filtered_data = [d for d in data if d.get('type') == agricultural_type]
-        return render_template('index.html', data=filtered_data, models=records, records_data=records_data, price_data=price_data, agricutural_type = agricultural_type)
+        # TODO: refactor this
+        return render_template('index.html', data=filtered_data,
+                               models=records, records_data=records_data,
+                               price_data=price_data, agricutural_type=agricultural_type,
+                               price_agricultural=price_agricultural,
+                               agricultural_type=AGRICULTURAL_TYPES,
+                               agricultural_mapping=AGRICULTURAL_TYPES_MAPPING)
 
-    return render_template('index.html', models=records, records_data=records_data, price_data=price_data, data=None, model_name="")
+    return render_template('index.html', models=records,
+                           records_data=records_data, price_data=price_data,
+                           data=None, model_name="", price_agricultural=price_agricultural,
+                           agricultural_type=AGRICULTURAL_TYPES,
+                           agricultural_mapping=AGRICULTURAL_TYPES_MAPPING)
+
+
+def get_price_with_date():
+    # lấy arima làm chuẩn vì arima chỉ có giá
+    arima_model = model.find_one({'name': 'ARIMA'})
+    attrs = arima_model.get('attrs')
+    if not attrs:
+        return {}
+    data = attrs.get('data', [])
+    price_agricultural = {}
+    for type in AGRICULTURAL_TYPES:
+        for d in data:
+            if d.get('type') == type:
+                data_csv = d.get('data')
+                df = pd.read_csv(data_csv)
+                if 'date' in df.columns:
+                    df['date'] = pd.to_datetime(df['date'])
+                today = datetime.date.today()
+                current_data = df[df['date'].dt.date == today]
+                if current_data.empty:
+                    current_data = df.iloc[-1]
+                current_data['date'] = current_data['date'].strftime('%d-%m-%Y')
+
+                price_agricultural[type] = current_data
+
+    return price_agricultural
 
 
 @main_router.route('/search-all-models', methods=['GET'])
@@ -65,6 +105,7 @@ def get_all_models():
         all_models.append(model_info)
     return jsonify(all_models)
 
+
 @main_router.route('/search-one-model', methods=['GET'])
 def get_data_train_model():
     model_name = request.args.get('model_name')
@@ -74,6 +115,7 @@ def get_data_train_model():
     data = model_data_find.get('attrs')
 
     return jsonify(data)
+
 
 @main_router.route('/register', methods=['GET', 'POST'])
 def register():
@@ -88,7 +130,7 @@ def register():
             return redirect('register')
 
         password = hashlib.md5(password.encode()).hexdigest()
-        
+
         users_collection = user
         # Check if the username already exists
         if users_collection.find_one({'username': username}):
@@ -99,6 +141,7 @@ def register():
             return redirect('login')
 
     return render_template('register.html')
+
 
 @main_router.route('/login', methods=['GET', 'POST'])
 def login():
@@ -120,9 +163,9 @@ def login():
 
     return render_template('login.html')
 
+
 @main_router.route('/logout', methods=['GET'])
 def logout():
-
     if session.get('is_authen'):
         session['is_authen'] = False
         del session['username']
